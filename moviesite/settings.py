aad6ef93b6d82aa02d7c0a73f3bb4165ca3bb89b
Adevaldo Junior <52014633+Pinto-Devas@users.noreply.github.com>
2024-09-29 21:53:33 -0300

    Adicionar modificações para implantação no Render

--- moviesite/settings.py
+++ moviesite/settings.py
@@ -26,6 +26,10 @@ SECRET_KEY = 'django-insecure-up86hra4xvw4q&bs*x@0*enmyt2p@kc61s5zzy=e@el)!)a#7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ["127.0.0.1", ".onrender.com"]
+
+CSRF_TRUSTED_ORIGINS = ["https://*.onrender.com/"]
+
 ALLOWED_HOSTS = []
 
 
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +125,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
