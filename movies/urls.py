154effe1f185d08509372f37f8aaa90453bbba25
Adevaldo Junior <52014633+Pinto-Devas@users.noreply.github.com>
2024-10-09 18:37:24 -0300

    Consertando build para realizar deploy no Render novamente

--- movies/urls.py
+++ movies/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.MovieListView.as_view(), name='index'),
     path('search/', views.search_movies, name='search'),
     path('create/', views.create_movie, name='create'),
-    path('/', views.detail_movie, name='detail'),
+    path('<int:movie_id>/', views.detail_movie, name='detail'),
     path('update/<int:movie_id>/', views.update_movie, name='update'),
     path('delete/<int:movie_id>/', views.delete_movie, name='delete'),
 ]
