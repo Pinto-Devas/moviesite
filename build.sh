70d8fe2671035e8f3d93ec4be460e0551d2f5dee
Adevaldo Junior <52014633+Pinto-Devas@users.noreply.github.com>
2024-10-09 21:57:42 -0300

    Atualizando o Build2x

--- build.sh
+++ build.sh
@@ -11,12 +11,16 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # create superuser if missing
-cat < | python manage.py shell
+python manage.py shell <<EOF
 import os
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
-    User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
+    User.objects.create_superuser(
+        os.environ["DJANGO_SUPERUSER_USERNAME"], 
+        os.environ["DJANGO_SUPERUSER_EMAIL"], 
+        os.environ["DJANGO_SUPERUSER_PASSWORD"]
+    )
 EOF
